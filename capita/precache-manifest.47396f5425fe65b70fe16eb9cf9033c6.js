aa1cd46b6023b6651a75584c978adf98deee7a1f
Christian Chown <christian.chown@santander.co.uk>
2019-08-28 14:49:15 +0100

    Capita coding exercise

---
+++ capita/precache-manifest.47396f5425fe65b70fe16eb9cf9033c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9917aaaa70bfa6e9b424b37b6ca3ca",
+    "url": "/capita/index.html"
+  },
+  {
+    "revision": "16b031b989ad5627bd2c",
+    "url": "/capita/static/css/main.f9290eab.chunk.css"
+  },
+  {
+    "revision": "cd0b4442c83e2068467c",
+    "url": "/capita/static/js/2.7411eda7.chunk.js"
+  },
+  {
+    "revision": "16b031b989ad5627bd2c",
+    "url": "/capita/static/js/main.c57afeb7.chunk.js"
+  },
+  {
+    "revision": "b3fd00b2878d67289c35",
+    "url": "/capita/static/js/runtime~main.5f9cef54.js"
+  }
+]);
