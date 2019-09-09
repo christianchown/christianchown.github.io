2a310af9fdb3f546f54a24efacac4ac788112ad3
Christian Chown <christian.chown@santander.co.uk>
2019-09-09 20:42:32 +0100

    onedrive

---
+++ onedrive/precache-manifest.58f927a6e3ddcc44e878dabd9d86c4b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3768b683625864bc9867b5bfdeac8630",
+    "url": "/onedrive/index.html"
+  },
+  {
+    "revision": "2cd9e6682a3134c49e6b",
+    "url": "/onedrive/static/css/main.68c44537.chunk.css"
+  },
+  {
+    "revision": "d68624a5d8995fa3563e",
+    "url": "/onedrive/static/js/2.4ddea556.chunk.js"
+  },
+  {
+    "revision": "2cd9e6682a3134c49e6b",
+    "url": "/onedrive/static/js/main.1231d59d.chunk.js"
+  },
+  {
+    "revision": "4d382dbaf42691d58dac",
+    "url": "/onedrive/static/js/runtime~main.a490ecf6.js"
+  },
+  {
+    "revision": "fa19bc21a3f0f22ad20df264375bb2c2",
+    "url": "/onedrive/static/media/folder.fa19bc21.svg"
+  }
+]);
