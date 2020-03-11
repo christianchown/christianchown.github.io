564955f7c63400d30cc9d702519842d13a3a53f8
Christian Chown <christianchown@gmail.com>
2020-03-11 09:25:10 +0000

    FiveAI corrected

---
+++ fiveai/precache-manifest.f24313349bf16f6c49938f84bafa7d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65a1ea70fbd5538a4f024d9ada449d3",
+    "url": "/fiveai/index.html"
+  },
+  {
+    "revision": "9244aa9dba60146f24aa",
+    "url": "/fiveai/static/css/main.cc1c65b9.chunk.css"
+  },
+  {
+    "revision": "1b7f135589e858a40e33",
+    "url": "/fiveai/static/js/2.4ecfd853.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/fiveai/static/js/2.4ecfd853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9244aa9dba60146f24aa",
+    "url": "/fiveai/static/js/main.74a412bc.chunk.js"
+  },
+  {
+    "revision": "11eec2c570c08e1ca8ce",
+    "url": "/fiveai/static/js/runtime-main.1c0d4b4c.js"
+  }
+]);
--- fiveai/service-worker.js
+++ fiveai/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capita/precache-manifest.37aec33056e59a071d6e8037ddf10919.js"
+  "/fiveai/precache-manifest.f24313349bf16f6c49938f84bafa7d7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/capita/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fiveai/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
